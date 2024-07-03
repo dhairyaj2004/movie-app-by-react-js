49e7e86bc16987612e73684ba560b4e0f2aadea4
Dhairya Shah <dhairyaj2004@gmail.com>
2024-07-03 17:03:24 +0530

    new

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 const Search = (props) => {
   return (
     <div className="Search-field">
-      <input
+      <input id="search-bar"
         type
-        placeholder="Search for movie"
+        placeholder="Search for Movie..."
         value={props.value}
         onChange={(e) => props.setSearchvalue(e.target.value)}
       />
+       
     </div>
   );
 };
