e9bb686996cfddd8b7075ddcb27e538774d89399
Dhairya Shah <dhairyaj2004@gmail.com>
2024-12-18 09:53:39 +0530

    updated error handling

--- src/App.js
+++ src/App.js
@@ -5,31 +5,38 @@ import SearchBox from "./SearchBox.js";
 import MovieTitle from "./MovieTitle";
 import fav from "./AddFav";
 import removefav from "./removefav";
+
 function App() {
   const [movies, setMovies] = useState([]);
   const [favourites, setFavourites] = useState([]);
   const [searchvalue, setSearchvalue] = useState("");
+
   const getMovieRequest = async () => {
-    const url = ` http://www.omdbapi.com/?s=${searchvalue}&apikey=ff2f2d80`;
+    const url = `http://www.omdbapi.com/?s=${searchvalue}&apikey=ff2f2d80`;
 
     const response = await fetch(url);
     const responseJson = await response.json();
+
     if (responseJson.Search) {
       setMovies(responseJson.Search);
     }
   };
+
   useEffect(() => {
     getMovieRequest();
   }, [searchvalue]);
+
   useEffect(() => {
-    const getFavourites = JSON.parse(localStorage.getItem("favourites"));
+    const getFavourites = JSON.parse(localStorage.getItem("favourites")) || []; 
     setFavourites(getFavourites);
-  });
+  }, []);
+
   const addfavmovie = (movie) => {
     const newfavlist = [...favourites, movie];
     setFavourites(newfavlist);
     savelocalstorage(newfavlist);
   };
+
   const removefavmovie = (movie) => {
     const updatedfavlist = favourites.filter(
       (favourite) => favourite.imdbID !== movie.imdbID
@@ -37,22 +44,23 @@ function App() {
     setFavourites(updatedfavlist);
     savelocalstorage(updatedfavlist);
   };
+
   const savelocalstorage = (items) => {
     localStorage.setItem("favourites", JSON.stringify(items));
   };
+
   return (
     <div className="wrap">
       <div className="header">
         <MovieTitle Title="📽️MOVIEMANIA🍿" />
-
-        <SearchBox className="searched_movies" searchvalue={searchvalue} setSearchvalue={setSearchvalue} />
+        <SearchBox
+          className="searched_movies"
+          searchvalue={searchvalue}
+          setSearchvalue={setSearchvalue}
+        />
       </div>
       <div className="row">
-        <MovieList
-          movies={movies}
-          handlefavclick={addfavmovie}
-          favmovie={fav}
-        />
+        <MovieList movies={movies} handlefavclick={addfavmovie} favmovie={fav} />
       </div>
       <div className="navbar">
         <MovieTitle Title="FAVOURITES❤️" />
--- src/MovieList.js
+++ src/MovieList.js
@@ -3,16 +3,21 @@ import "./App.js";
 
 const MovieList = (props) => {
   const FavMovie = props.favmovie;
+
   return (
     <>
-      {props.movies.map((movie, index) => (
-        <div className="poster">
-          <img src={movie.Poster} alt="movie"></img>
-          <div className="overlay" onClick={() => props.handlefavclick(movie)}>
-            <FavMovie />
+      {props.movies && props.movies.length > 0 ? (
+        props.movies.map((movie, index) => (
+          <div className="poster" key={index}>
+            <img src={movie.Poster} alt="movie"></img>
+            <div className="overlay" onClick={() => props.handlefavclick(movie)}>
+              <FavMovie />
+            </div>
           </div>
-        </div>
-      ))}
+        ))
+      ) : (
+        <p>No movies available</p>
+      )}
     </>
   );
 };
